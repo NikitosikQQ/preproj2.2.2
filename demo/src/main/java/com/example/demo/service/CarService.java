e2db532d771298ddd30461bb12c5d6e93a86a041
NikitosikQQ <120980323+NikitosikQQ@users.noreply.github.com>
2023-04-17 18:47:48 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/service/CarService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.car.Car;
+import com.example.demo.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> getListOfCars() {
+        return carRepository.findAll();
+    }
+
+    public List<Car> getListOfCarsByCount(Long count, Long maxCars) {
+        if (count == null || count >= maxCars) {
+            return getListOfCars();
+        } else {
+            return getListOfCars().stream().limit(count).toList();
+        }
+    }
+}
